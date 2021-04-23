955f817539152d5f8b0f310af43672711d31accd
wadep <wadepfacer@gmail.com>
2021-04-23 09:28:13 -0600

    polished fetch API

--- js/script.js
+++ js/script.js
@@ -1,37 +1,58 @@
 var userInput = document.querySelector("#city");
 var userFormEl = document.querySelector('#user-form');
 
-
-// var userWeatherApi = "https://api.openweathermap.org/data/2.5/onecall?appid=75011bac43d9fd1067de54fbcd6e3d47&" 
-// var userCity = function(city){
+var userCity = function(city){
     
-//     var getCoordApi = "https://api.openweathermap.org/data/2.5/weather?appid=75011bac43d9fd1067de54fbcd6e3d47&q=" + userInput;
-//         fetch(getCoordApi)
-//             .then(function(response){
-//                 if (response.ok){
-//                     response.json()
-//             .then(function(data){
-//                     console.log(data);
-//                 });
-//             }else{
-//                 alert('Error: ' + response.statusText);
-//             }
-//         })
-//         .catch(function(error){
-//             alert("Unable to connect to Weather API");
-//         });            
-// };
+    var getCoordApi = 
+    "https://api.openweathermap.org/data/2.5/weather?appid=75011bac43d9fd1067de54fbcd6e3d47&q=" 
+    + userInput.value.trim(' ');
+    fetch(getCoordApi)
+    .then(function(response){
+        if (response.ok){
+            response.json()
+            .then(function(data){
+                lon = data.coord.lon;
+                lat = data.coord.lat;
+                console.log(lon);
+                console.log(lat);
+                userWeather();
+            });
+        }else{
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function(error){
+        alert("Unable to connect to Weather API");
+    });            
+};
 
-var formSubmitHandler = function(e){
-    e.preventDefault();
+var userWeather = function(weather){
+    var userWeatherApi = "https://api.openweathermap.org/data/2.5/onecall?appid=75011bac43d9fd1067de54fbcd6e3d47&lat=" 
+    + lat + "&lon=" + lon;
+    fetch(userWeatherApi)
+    .then(function(response){
+        if(response.ok){
+            response.json()
+            .then(function(data){
+                console.log(data);
+            })
+        }else{
+            console.log("Error in fetch function!");
+        }
+    })
+}  
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
     var city = userInput.value;
+    // console.log(city);
     
     if (city){
-        usercity(city);
-        userInput.value = "";                
-        console.log(userInput);    
+        userCity(city);
+        userInput.value = "";    
     }else{
         alert('Please enter a City');
     }
 };
-userFormEl.addEventListener('submit', formSubmitHandler);
+$("#submit").on("click", formSubmitHandler);
+
