6f26050e157a55dbbb4cb90f2c0eab53ffbb195c
wadep <wadepfacer@gmail.com>
2021-04-23 20:26:27 -0600

    added function to populate weather data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-var userInput = document.querySelector("#city");
+var userInput = document.querySelector("#city-name");
 var userFormEl = document.querySelector('#user-form');
+var recentSearch = document.querySelector('#recent-search');
+var currentWeather = document.querySelector('#details');
+var currentDate = document.querySelector('#date');
+var currentCity = document.querySelector('#city');
+var curTemp = document.querySelector('#temp');
+var curWind = document.querySelector('#wind');
+var curHumid = document.querySelector('#humid');
+var curUv = document.querySelector('#uv');
+var icon = document.querySelector('#icon');
+
+
+
 
 var userCity = function(city){
     
@@ -11,10 +23,13 @@ var userCity = function(city){
         if (response.ok){
             response.json()
             .then(function(data){
+                // create variables for longitude and lattitude
                 lon = data.coord.lon;
                 lat = data.coord.lat;
-                console.log(lon);
-                console.log(lat);
+                // create city variable
+                cityName = data.name;
+                console.log(cityName);
+                console.log(data);
                 userWeather();
             });
         }else{
@@ -23,29 +38,52 @@ var userCity = function(city){
     })
     .catch(function(error){
         alert("Unable to connect to Weather API");
-    });            
+    });
 };
 
 var userWeather = function(weather){
     var userWeatherApi = "https://api.openweathermap.org/data/2.5/onecall?appid=75011bac43d9fd1067de54fbcd6e3d47&lat=" 
-    + lat + "&lon=" + lon;
+    + lat + "&lon=" + lon + "&units=imperial";
     fetch(userWeatherApi)
     .then(function(response){
         if(response.ok){
             response.json()
             .then(function(data){
-                console.log(data);
+                // create variabels for weather conditions
+                temp = data.current.temp;
+                humid = data.current.humidity;
+                wind = data.current.wind_speed;
+                uv = data.current.uvi;
+                weatherIcon = data.current.weather[3];                
+                console.log(data);                
+                // Populate values to HTML
+                showWeather();
             })
         }else{
             console.log("Error in fetch function!");
         }
     })
-}  
+    // Show variables on html    
+    
+    
+    
+    
+};
+var showWeather = function(){
 
+    currentDate.textContent = moment().format('dddd, MMMM Do');    
+    currentCity.textContent = cityName;
+    curTemp.textContent = temp;
+    curWind.innerHTML = wind;
+    curHumid.innerHTML = humid;
+    curUv.textContent = uv;
+    icon.textContent = weatherIcon;
+    console.log(weatherIcon);
+}
+// City submit on click function
 var formSubmitHandler = function(event){
     event.preventDefault();
-    var city = userInput.value;
-    // console.log(city);
+    var city = userInput.value;   
     
     if (city){
         userCity(city);
@@ -54,5 +92,8 @@ var formSubmitHandler = function(event){
         alert('Please enter a City');
     }
 };
+
+
+// On click 
 $("#submit").on("click", formSubmitHandler);
 
